f3d78a84dc917f56380205e40d6750a90e7274c6
nightowl-devs <151651325+nightowl-devs@users.noreply.github.com>
2023-12-23 16:34:20 +0100

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -4,3 +4,10 @@ sudo apt-get update
 sudo apt-get upgrade 
 sudo apt install python3-pip
 sudo pip install cloudscraper requests PySocks icmplib scapy
+sudo ufw allow 22
+sudo ufw allow 5512
+sudo ufw allow 5511
+sudo ufw allow 1:5655/tcp
+sudo ufw allow 1:5655/tcp
+sudo ufw enable
+sudo ufw status
