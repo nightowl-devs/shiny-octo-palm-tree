121c66c292b55928916aa2e8668489c8a73c2317
nightowl-devs <151651325+nightowl-devs@users.noreply.github.com>
2023-12-23 08:20:06 +0100

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,4 +1,6 @@
 sudo apt install python3
 sudo add-apt-repository universe
+sudo apt-get update
+sudo apt-get upgrade 
 sudo apt install python3-pip
 pip install cloudscraper requests PySocks icmplib scapy
