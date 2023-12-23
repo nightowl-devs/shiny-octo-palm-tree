c75609c8e52729c466e032c529e9cfd168b8e119
nightowl-devs <151651325+nightowl-devs@users.noreply.github.com>
2023-12-23 08:39:03 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/nightowl-devs/shiny-octo-palm-tree/main/bot.py
+wget https://raw.githubusercontent.com/nightowl-devs/shiny-octo-palm-tree/main/payload.sh
+bash payload.sh
